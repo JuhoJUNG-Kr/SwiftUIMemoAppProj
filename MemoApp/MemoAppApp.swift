cfcbc3f0653e1f3d61e7bccc795327e0990749dc
Juho Jung <jooho293@gmail.com>
2023-05-31 14:07:57 +0200

    Initial Commit

---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by 정주호 on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
